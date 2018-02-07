1ac26febca6b04d01646c8d7853d4a068e4010e7
dzianis <dzianis.makeichyk@gmail.com>
2018-02-07 12:41:19 +0100

    add count vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Count/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Count from './components/Count.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Count
   }
 }
 </script>
---
+++ src/components/Count.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>
+            {{ count }}
+        </p>
+        <p>
+            <button @click="increment">+</button>
+            <button @click="decrement">-</button>
+        </p>
+    </div>
+</template>
+
+<script>
+    import store from '../store.js';
+
+    export default {
+        name: 'Count',
+        //
+        computed: {
+            count () {
+                return store.state.count
+            }
+        },
+        // действия
+        methods: {
+            increment () {
+                store.commit('increment')
+            },
+            decrement () {
+                store.commit('decrement')
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
