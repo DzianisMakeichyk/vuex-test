2584773237dba1250da2946128f6330a1b11766f
dzianis <dzianis.makeichyk@gmail.com>
2018-02-08 13:43:16 +0100

    add vrouter

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,33 @@
   <div id="app">
     <img src="./assets/logo.png">
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <Count/>
+    <p>
+      <router-link :to="{name: 'Count'}">Count</router-link>
+      <router-link :to="{name: 'Animation'}">Animation</router-link>
+    </p>
+    <router-view>
+
+    </router-view>
+    <!--<Count/>-->
+    <!--<Animation/>-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Count from './components/Count.vue'
+import Animation from './components/animation.vue'
 import store from './store'
+import router from './router/index'
 
 export default {
     name: 'app',
     store,
+    router,
     components: {
       HelloWorld,
-      Count
+      Count,
+      Animation
    }
 }
 </script>
