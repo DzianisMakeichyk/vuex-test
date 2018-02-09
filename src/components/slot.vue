436d7fcfe7721356f8c05c6a206b6c5f502b6a38
dzianis <dzianis.makeichyk@gmail.com>
2018-02-09 13:32:32 +0100

    add gsap

--- src/components/slot.vue
+++ src/components/slot.vue
@@ -1,14 +1,10 @@
 <template>
-    <div class="page " v-bind:class="{ slide: this.$store.state.isActive }">
-        <div class="center">
-            <p class="text">
-              <span class="text__first">
-                <span class="text__word">
-                  <slot></slot>
-                </span>
-                <span class="text__first-bg"></span>
-              </span>
-            </p>
-        </div>
-    </div>
+    <p class="text">
+      <span class="text__first">
+        <span class="text__word">
+          <slot></slot>
+        </span>
+        <span class="text__first-bg"></span>
+      </span>
+    </p>
 </template>
